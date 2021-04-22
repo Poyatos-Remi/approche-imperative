ffb5fb1a47af748893eae8f1bc92140aae2c0828
Poyatos Rémi <poyatosremi@gmail.com>
2021-04-22 15:06:56 +0200

    v1

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -4,7 +4,16 @@ public class DeclarationApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		byte var1 = 2;
+		short var2 = 4;
+		int var3 = 6;
+		long var4 = 42L;
+		float var5 = 42.5F;
+		double var6 = 42.6D;
+		char car1 = 'a';
+		boolean bool = true;
+		String randomString = "Voici le résultat d'un calcul : \n 1+5=6";
+		System.out.println(randomString);
 	}
 
 }
